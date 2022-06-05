62a135f825a2209a8301651801add635797b0c75
P1char <64097856+P1char@users.noreply.github.com>
2022-06-05 12:50:14 +0200

    Add files via upload

---
+++ generation_fichier.py
@@ -0,0 +1,63 @@
+import xlrd
+import sys
+import os
+
+
+document = xlrd.open_workbook("Activites.xlsx")
+feuille_1 = document.sheet_by_index(0)
+rows = feuille_1.nrows
+cols = feuille_1.ncols
+
+f=open("Archive_activite.txt","w+")
+villeid=int(sys.argv[0])
+ville = feuille_1.cell_value(villeid,1)
+f.write(ville)
+
+for i in range(8, cols):
+    cellule = feuille_1.cell_value(villeid,i) #rows=i ; colx=1
+    if cellule!='':
+        activite=feuille_1.cell_value(7,i)
+        f.write("\n- %s \n" %(activite))
+f.close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document = xlrd.open_workbook(lien1)
+feuille_1 = document.sheet_by_index(0)
+rows = feuille_1.nrows
+cols = feuille_1.ncols
+print("MARIE LA FRANCOISE")
+for i in range(8, rows):
+    cellule = feuille_1.cell_value(i,1) #rows=i ; colx=1
+    if cellule!='':
+        print("%d : %s" %(i,cellule))
+
+
+
+
+
+
+
+
+
+
+
+
