e94f3fc96ec6e79a7ad1a70d5de984806ba0eead
P1char <64097856+P1char@users.noreply.github.com>
2022-06-05 12:50:25 +0200

    Add files via upload

---
+++ etape1_generation.py
@@ -0,0 +1,16 @@
+import xlrd
+print("odfjgsdmlgjqsmlfkjqsrgmojsdhvpoqivbpqciru bpjrhsljkfsRFHZRLKJGSRGJ")
+document = xlrd.open_workbook("Activites.xlsx")
+feuille_1 = document.sheet_by_index(0)
+rows = feuille_1.nrows
+cols = feuille_1.ncols
+f=open("select_city.php","w+")
+f.write('<!DOCTYPE html>\n<html>\n<head>\n  <meta charset="utf-8">\n  <title>Activite par villes</title>\n')
+f.write('<FORM method="post" action="welcome.php"> \n<SELECT id="select_city" name="Villes" size="1">\n')
+for i in range(8, rows):
+    cellule = feuille_1.cell_value(i,1) #rows=i ; colx=1
+    if cellule!='':
+        f.write("<option value='%d'>%s</option>\n" %(i,cellule))
+
+f.write('</SELECT> \n<input type="submit" value="VALIDER"> \n</FORM>\n')
+
